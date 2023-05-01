21db4314baa2a8f4f536d63755d03ef4c4829009
znajman <79368320+znajman@users.noreply.github.com>
2023-05-01 15:55:45 +0200

    Implement resize option

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.body.onload = addDrawingArea;
+    document.body.onload = addDrawingArea();
 
+    const resize = document.querySelector('.resize');
+    resize.addEventListener('click', resizeDrawArea);
 });
 
 // create drawing area
-function addDrawingArea() {
+function addDrawingArea(n = 10) {
     
     // select where to place div in the DOM
     const drawArea = document.querySelector('.draw-area');
     
     
     // create drawing area
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < n; i++) {
 
         // create a row of divs
         const newRow = document.createElement('div');
         newRow.classList.add('row');
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < n; i++) {
             // create new div
             const newDiv = document.createElement('div');
             newDiv.classList.add('pixel');
@@ -25,6 +27,7 @@ function addDrawingArea() {
 
         drawArea.appendChild(newRow);
     }
+
     draw();
 }
 
@@ -43,4 +46,25 @@ function draw() {
             pixel.style.backgroundColor = 'black';
         });
     });
+}
+
+// delete the existing draw area and create a new one with prompted size
+function resizeDrawArea() {
+    let n = prompt('Select size:');
+    while( !Number.isInteger(+n) && n < 100 && n > 0) {
+        console.log(n);
+        n = prompt('Select size:');
+    }
+    deleteDrawingArea();
+    addDrawingArea(n);
+}
+
+// delete the draw area
+function deleteDrawingArea() {
+    // select div to delete 
+    const drawArea = document.querySelector('.draw-area');
+
+    while (drawArea.firstChild) {
+        drawArea.removeChild(drawArea.firstChild);
+    }
 }
